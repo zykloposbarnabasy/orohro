1ddc3944e0c061f18ee6d43e2ec8cda8ca246f8f
Zyklopos Barnabasy <zyklopos.barnabasy@gmail.com>
2021-10-24 12:10:30 +1100

    use stylesheet pack / production redis

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,9 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import '../../assets/stylesheets/application.css'
+import '../../assets/stylesheets/products.scss'
+
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
