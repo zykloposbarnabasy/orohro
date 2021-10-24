88d6ae4dc949e9401fa01563c059febe60e8277b
Zyklopos Barnabasy <zyklopos.barnabasy@gmail.com>
2021-10-24 17:56:15 +1100

    seed another product and some reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,22 @@
-Product.create(
-  slug: 'sample-product',
-  title: 'The Sample Product'
-)
+products = [
+  ['sample-product', 'The Sample Product'],
+  ['second-sample-product', 'Sample Product №2']
+]
+
+products.each do |slug, title|
+  Product.create(slug: slug, title: title)
+end
+
+
+reviews = [
+  [1, 5, 'Amazing! This must be one of my favourite products. I am using it all the time. Best buy in a long time.'],
+  [1, 3, 'Fair.'],
+  [1, 2, 'Wasn\'t really satisfied with it.'],
+  [2, 3, 'It\'s okay but it could use some improvements. Usability is lacking in some places. Overall decent product.'],
+  [2, 1, 'I regret ever looking at it.'],
+  [2, 4, 'Quite good!']
+]
+
+reviews.each do |product_id, score, text|
+  Review.create(product_id: product_id, score: score, text: text)
+end
